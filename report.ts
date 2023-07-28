cbd07e738e1b2f95a3ab90fea85df89d7de697c8
Dominic Zappala <dominic.zappala@gmail.com>
2023-07-28 12:49:47 -0400

    feat: remove unnecessary code

--- report.ts
+++ report.ts
@@ -17,12 +17,4 @@ export default {
         .setDescription("The description of the issue")
         .setRequired(true)
     ),
-  // async execute(interaction: CommandInteraction) {
-  //   interaction.showModal(
-  //     getModal(
-  //       interaction.options.data[0].value as string, // ["issueTitle"]
-  //       interaction.options.data[1].value as string // ["issueDescription"]
-  //     )
-  //   );
-  // },
 };
