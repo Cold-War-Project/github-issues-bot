85b7c76bcc76fec23f2aceb7ab67103d6ba3c4dd
Dominic Zappala <dominic.zappala@gmail.com>
2023-07-28 12:47:12 -0400

    test: commented unnecessary code

--- report.ts
+++ report.ts
@@ -17,12 +17,12 @@ export default {
         .setDescription("The description of the issue")
         .setRequired(true)
     ),
-  async execute(interaction: CommandInteraction) {
-    interaction.showModal(
-      getModal(
-        interaction.options.data[0].value as string, // ["issueTitle"]
-        interaction.options.data[1].value as string // ["issueDescription"]
-      )
-    );
-  },
+  // async execute(interaction: CommandInteraction) {
+  //   interaction.showModal(
+  //     getModal(
+  //       interaction.options.data[0].value as string, // ["issueTitle"]
+  //       interaction.options.data[1].value as string // ["issueDescription"]
+  //     )
+  //   );
+  // },
 };
